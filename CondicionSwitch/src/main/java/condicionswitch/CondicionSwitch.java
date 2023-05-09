781895816b28f8598bfcbb1caa1a020cc0923466
diegoarriagadazamora <diegoarriagadazamora@gmail.com>
2023-05-08 20:34:29 -0400

    Practicas Java con TodoCode

---
+++ CondicionSwitch/src/main/java/condicionswitch/CondicionSwitch.java
@@ -0,0 +1,35 @@
+package condicionswitch;
+
+public class CondicionSwitch {
+
+    public static void main(String[] args) {
+        int dia =4;
+        String nombreDia;
+        
+        switch(dia){
+            case 1: nombreDia = "Lunes";
+            break;
+            case 2: nombreDia = "Martes";
+            break;
+            case 3: nombreDia = "Miércoles";
+            break;
+            case 4: nombreDia = "Jueves";
+            break;
+            case 5: nombreDia = "Viernes";
+            break;
+            case 6: nombreDia = "Sábado";
+            break;
+            case 7: nombreDia = "Domingo";
+            break;
+            default: nombreDia = "Número de Día invalido";
+        }
+        
+        //se ha creado un if else distinto a lo enseñado en todocode con operador ||
+        if(dia==1||dia==2||dia==3||dia==4||dia==5||dia==6||dia==7){
+        System.out.println("El Día de la semana es; " + nombreDia);
+        }
+        else{
+            System.out.println(nombreDia);
+        }
+    }
+}
---
+++ EjercicioCondicionales/src/main/java/ejerciciocondicionales/EjercicioCondicionales.java
@@ -0,0 +1,34 @@
+package ejerciciocondicionales;
+import java.util.Scanner;
+
+public class EjercicioCondicionales {
+
+    public static void main(String[] args) {
+        
+        double sueldo=0;
+        int categoria;
+        
+        System.out.println("Ingrese el tipo de categoria que desea calcular el sueldo");
+        Scanner teclado = new Scanner (System.in);
+        categoria = teclado.nextInt();
+                if(categoria==1){
+                    sueldo = 15890 + (15890*0.10);
+                }
+                else{
+                    if(categoria==2){
+                        sueldo = 25630.89;
+                    }
+                    else{
+                        if(categoria==3){
+                            sueldo = 35560.20 + (35560.20*0.11);
+                        }
+                        else{
+                            System.out.println("Debe ingresar un número de categoría valida");
+                        }
+                    }
+                }
+                if(categoria==1||categoria==2||categoria==3){
+                System.out.println("El total de sueldo para la categoría seleccionada " + categoria + " Es igual a: " + sueldo);
+                }
+    }
+}
